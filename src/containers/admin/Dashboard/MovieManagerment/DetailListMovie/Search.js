7e39594483e96498d766d8242dcf96b3067ac42b
Thanhvu2611 <35416729+Thanhvu2611@users.noreply.github.com>
2020-10-12 12:43:11 +0700

    update

--- src/containers/admin/Dashboard/MovieManagerment/DetailListMovie/Search.js
+++ src/containers/admin/Dashboard/MovieManagerment/DetailListMovie/Search.js
@@ -1,29 +1,35 @@
-import React, { Component } from 'react';
+import React, { Component } from "react";
 import { connect } from "react-redux";
 import { actGetKeyWordListMovie } from "./modules/action";
 
 class Search extends Component {
   render() {
     return (
-      <div className="input-group mb-3" >
-        <input type="text" className="form-control" placeholder="Search" onChange={event => {
-          this.props.getKeyWordListMovie(event.target.value);
-        }} />
+      <div className="input-group mb-3">
+        <input
+          type="text"
+          className="form-control"
+          placeholder="Search"
+          onChange={(event) => {
+            this.props.getKeyWordListMovie(event.target.value);
+          }}
+        />
         <div className="input-group-append">
-          <button className="btn btn-success" type="submit">Search</button>
+          <button className="btn btn-success" type="submit">
+            Search
+          </button>
         </div>
       </div>
-
-    )
+    );
   }
 }
 
-const mapDispatchToProps = dispatch => {
+const mapDispatchToProps = (dispatch) => {
   return {
     getKeyWordListMovie: (keyword) => {
       dispatch(actGetKeyWordListMovie(keyword));
-    }
-  }
-}
+    },
+  };
+};
 
 export default connect(null, mapDispatchToProps)(Search);
