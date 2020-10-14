416914274b6a307d824866dacb7ebc0ed92fb62c
Long Ho <recca.ag>
2020-10-14 13:57:41 +0700

    update

--- src/containers/admin/Dashboard/index.js
+++ src/containers/admin/Dashboard/index.js
@@ -1,25 +1,25 @@
-import React, { Component } from 'react';
-import { NavLink } from "react-router-dom";
+// import React, { Component } from 'react';
+// import { NavLink } from "react-router-dom";
 
 
 
 
-export default class Dashboard extends Component {
-  render() {
-    return (
-      <div className="row" >
+// export default class Dashboard extends Component {
+//   render() {
+//     return (
+//       <div className="row" >
 
-        <ul className="nav flex-column">
-          <li className="nav-item">
-            <NavLink activeClassName="active" className="nav-link" to="/admin/movie">Quản Lý Phim</NavLink>
-          </li>
-          <li className="nav-item">
-            <NavLink activeClassName="active" className="nav-link" to="/admin/user">Quản Lý Người Dùng</NavLink>
-          </li>
+//         <ul className="nav flex-column">
+//           <li className="nav-item">
+//             <NavLink activeClassName="active" className="nav-link" to="/admin/movie">Quản Lý Phim</NavLink>
+//           </li>
+//           <li className="nav-item">
+//             <NavLink activeClassName="active" className="nav-link" to="/admin/user">Quản Lý Người Dùng</NavLink>
+//           </li>
 
-        </ul>
+//         </ul>
 
-      </div>
-    )
-  }
-}
+//       </div>
+//     )
+//   }
+// }
